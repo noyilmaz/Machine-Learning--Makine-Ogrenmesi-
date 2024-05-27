1d939d7fecbda042fc5bc412b15b46fc71770abe
noyilmaz <oguzhan111111111@hotmail.com>
2024-05-28 02:41:15 +0300

    Add files via upload

---
+++ Makine Ogrenmesi/4-Sınıflandırma Algoritmaları/DecisionTreeClassifier.py
@@ -0,0 +1,195 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+import os
+print(os.listdir("../input"))
+
+# We are reading our data
+df = pd.read_csv("../input/heart.csv")
+
+# First 5 rows of our data
+df.head()
+#data keşfi
+df.target.value_counts()
+sns.countplot(x="target", data=df, palette="bwr")
+plt.show()
+countNoDisease = len(df[df.target == 0])
+countHaveDisease = len(df[df.target == 1])
+print("Percentage of Patients Haven't Heart Disease: {:.2f}%".format((countNoDisease / (len(df.target))*100)))
+print("Percentage of Patients Have Heart Disease: {:.2f}%".format((countHaveDisease / (len(df.target))*100)))
+sns.countplot(x='sex', data=df, palette="mako_r")
+plt.xlabel("Sex (0 = female, 1= male)")
+plt.show()
+
+df.groupby('target').mean()
+
+pd.crosstab(df.age,df.target).plot(kind="bar",figsize=(20,6))
+plt.title('Heart Disease Frequency for Ages')
+plt.xlabel('Age')
+plt.ylabel('Frequency')
+plt.savefig('heartDiseaseAndAges.png')
+plt.show()
+
+pd.crosstab(df.sex,df.target).plot(kind="bar",figsize=(15,6),color=['#1CA53B','#AA1111' ])
+plt.title('Heart Disease Frequency for Sex')
+plt.xlabel('Sex (0 = Female, 1 = Male)')
+plt.xticks(rotation=0)
+plt.legend(["Haven't Disease", "Have Disease"])
+plt.ylabel('Frequency')
+plt.show()
+
+plt.scatter(x=df.age[df.target==1], y=df.thalach[(df.target==1)], c="red")
+plt.scatter(x=df.age[df.target==0], y=df.thalach[(df.target==0)])
+plt.legend(["Disease", "Not Disease"])
+plt.xlabel("Age")
+plt.ylabel("Maximum Heart Rate")
+plt.show()
+
+pd.crosstab(df.slope,df.target).plot(kind="bar",figsize=(15,6),color=['#DAF7A6','#FF5733' ])
+plt.title('Heart Disease Frequency for Slope')
+plt.xlabel('The Slope of The Peak Exercise ST Segment ')
+plt.xticks(rotation = 0)
+plt.ylabel('Frequency')
+plt.show()
+
+pd.crosstab(df.fbs,df.target).plot(kind="bar",figsize=(15,6),color=['#FFC300','#581845' ])
+plt.title('Heart Disease Frequency According To FBS')
+plt.xlabel('FBS - (Fasting Blood Sugar > 120 mg/dl) (1 = true; 0 = false)')
+plt.xticks(rotation = 0)
+plt.legend(["Haven't Disease", "Have Disease"])
+plt.ylabel('Frequency of Disease or Not')
+plt.show()
+
+pd.crosstab(df.cp,df.target).plot(kind="bar",figsize=(15,6),color=['#11A5AA','#AA1190' ])
+plt.title('Heart Disease Frequency According To Chest Pain Type')
+plt.xlabel('Chest Pain Type')
+plt.xticks(rotation = 0)
+plt.ylabel('Frequency of Disease or Not')
+plt.show()
+
+a = pd.get_dummies(df['cp'], prefix = "cp")
+b = pd.get_dummies(df['thal'], prefix = "thal")
+c = pd.get_dummies(df['slope'], prefix = "slope")
+
+frames = [df, a, b, c]
+df = pd.concat(frames, axis = 1)
+df.head()
+
+df = df.drop(columns = ['cp', 'thal', 'slope'])
+df.head()
+
+y = df.target.values
+x_data = df.drop(['target'], axis = 1)
+
+# Normalize
+x = (x_data - np.min(x_data)) / (np.max(x_data) - np.min(x_data)).values
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2,random_state=0)
+
+#transpose matrices
+x_train = x_train.T
+y_train = y_train.T
+x_test = x_test.T
+y_test = y_test.T
+
+#initialize
+def initialize(dimension):
+    
+    weight = np.full((dimension,1),0.01)
+    bias = 0.0
+    return weight,bias
+
+def sigmoid(z):
+    
+    y_head = 1/(1+ np.exp(-z))
+    return y_head
+    def forwardBackward(weight,bias,x_train,y_train):
+    # Forward
+    
+    y_head = sigmoid(np.dot(weight.T,x_train) + bias)
+    loss = -(y_train*np.log(y_head) + (1-y_train)*np.log(1-y_head))
+    cost = np.sum(loss) / x_train.shape[1]
+    
+    # Backward
+    derivative_weight = np.dot(x_train,((y_head-y_train).T))/x_train.shape[1]
+    derivative_bias = np.sum(y_head-y_train)/x_train.shape[1]
+    gradients = {"Derivative Weight" : derivative_weight, "Derivative Bias" : derivative_bias}
+    
+    return cost,gradients
+
+def update(weight,bias,x_train,y_train,learningRate,iteration) :
+    costList = []
+    index = []
+    
+    #for each iteration, update weight and bias values
+    for i in range(iteration):
+        cost,gradients = forwardBackward(weight,bias,x_train,y_train)
+        weight = weight - learningRate * gradients["Derivative Weight"]
+        bias = bias - learningRate * gradients["Derivative Bias"]
+        
+        costList.append(cost)
+        index.append(i)
+
+    parameters = {"weight": weight,"bias": bias}
+    
+    print("iteration:",iteration)
+    print("cost:",cost)
+
+    plt.plot(index,costList)
+    plt.xlabel("Number of Iteration")
+    plt.ylabel("Cost")
+    plt.show()
+
+    return parameters, gradients
+
+def predict(weight,bias,x_test):
+    z = np.dot(weight.T,x_test) + bias
+    y_head = sigmoid(z)
+
+    y_prediction = np.zeros((1,x_test.shape[1]))
+    
+    for i in range(y_head.shape[1]):
+        if y_head[0,i] <= 0.5:
+            y_prediction[0,i] = 0
+        else:
+            y_prediction[0,i] = 1
+    return y_prediction
+
+def logistic_regression(x_train,y_train,x_test,y_test,learningRate,iteration):
+    dimension = x_train.shape[0]
+    weight,bias = initialize(dimension)
+    
+    parameters, gradients = update(weight,bias,x_train,y_train,learningRate,iteration)
+
+    y_prediction = predict(parameters["weight"],parameters["bias"],x_test)
+    
+    print("Manuel Test Accuracy: {:.2f}%".format((100 - np.mean(np.abs(y_prediction - y_test))*100)))
+
+logistic_regression(x_train,y_train,x_test,y_test,1,100)
+
+accuracies = {}
+
+lr = LogisticRegression()
+lr.fit(x_train.T,y_train.T)
+acc = lr.score(x_test.T,y_test.T)*100
+
+accuracies['Logistic Regression'] = acc
+print("Test Accuracy {:.2f}%".format(acc))
+
+from sklearn.svm import SVC
+
+from sklearn.tree import DecisionTreeClassifier
+dtc = DecisionTreeClassifier()
+dtc.fit(x_train.T, y_train.T)
+
+acc = dtc.score(x_test.T, y_test.T)*100
+accuracies['Decision Tree'] = acc
+print("Decision Tree Test Accuracy {:.2f}%".format(acc))
+print("Maximum KNN Score is {:.2f}%".format(acc))
