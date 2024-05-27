760adbf999d259384632e837c2649cb97ad1e6e5
noyilmaz <oguzhan111111111@hotmail.com>
2024-05-28 02:41:56 +0300

    Add files via upload

---
+++ Makine Ogrenmesi/2-Makine öğrenmesi veri işlemleri/kutuphaneTanimlama.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+import os
+print(os.listdir("../input"))
